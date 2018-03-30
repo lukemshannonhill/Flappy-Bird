7144682acd743be7892e33046bd56818abd74cd9
Nishant Thakur <nt.nishantt@gmail.com>
2018-03-30 13:04:03 -0500

    Initial commit

--- neural_network.py
+++ neural_network.py
@@ -9,14 +9,13 @@ class NeuralNetwork:
         self.output_nodes = output_nodes
 
         # generate weight and bias matrices using random function
-        self.weights_IH = np.random.rand(self.hidden_nodes, self.input_nodes)
-        self.weights_HO = np.random.rand(self.output_nodes, self.hidden_nodes)
+        self.weights_IH = 2.5 * np.random.rand(self.hidden_nodes, self.input_nodes) - 1.25
+        self.weights_HO = 2.5 * np.random.rand(self.output_nodes, self.hidden_nodes) - 1.25
 
         self.bias_H = np.random.rand(self.hidden_nodes, 1)
         self.bias_O = np.random.rand(self.output_nodes, 1)
 
-
-        self.learning_rate = 0.2;
+        self.learning_rate = 0.02
 
 
     def sigmoid(self, x):
@@ -28,9 +27,9 @@ class NeuralNetwork:
         return x * (1 - x)
 
 
-    def feedforward(self,input_list):
+    def predict(self,input_list):
         # Convert the input list to a 2x1 numpy array/matrix
-        input_matrix = np.reshape(np.array(input_list),(-1,1))
+        input_matrix = np.reshape(np.array(input_list, dtype='float'),(-1,1))
 
         # Generating the hidden outputs
         hidden_matrix = np.matmul(self.weights_IH, input_matrix) + self.bias_H
@@ -43,22 +42,20 @@ class NeuralNetwork:
 
         #Activation function
         output_matrix = self.sigmoid(output_matrix)
-
         return hidden_matrix, output_matrix
 
 
     def train(self, input_list, target_list):
 
         # get output from the feedforward NN
-        hidden_matrix, outputs_matrix = self.feedforward(input_list)
+        hidden_matrix, outputs_matrix = self.predict(input_list)
 
         # convert list to numpy array
-        input_matrix = np.reshape(np.array(input_list),(-1,1))
-        targets = np.array(target_list)
+        input_matrix = np.reshape(np.array(input_list, dtype="float"),(-1,1))
+        targets = np.reshape(np.array(target_list, dtype="float"),(-1,1))
 
         #calculate the error
         output_error = targets - outputs_matrix
-        # print(targets,"-",str(outputs_matrix),"=",output_error)
 
         #calculate output gradients
         outputs_gradients = self.derivative_siggmoid(outputs_matrix) * output_error * self.learning_rate
@@ -85,4 +82,4 @@ class NeuralNetwork:
         self.weights_IH = self.weights_IH + delta_weights_IH
 
         # Adjust the hidden bias by delta
-        self.bias_H = self.bias_H + outputs_gradients
+        self.bias_H = self.bias_H + hidden_gradients
